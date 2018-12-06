0a1b54e9f42d439ca50b1f27d1fe9d6e6df062fd
Yuliia Lepska <iuliia.chumachenko@gmail.com>
2018-12-06 08:55:56 +0200

    polyfill promise all

---
+++ promiseAll.js
@@ -0,0 +1,34 @@
+Promise.all = function(promiseList){
+    return new Promise(function(resolve, reject) {
+        var fullfild = [];
+        var countFullfild = 0;
+        function checkResolving(){
+            countFullfild++;
+            if(countFullfild === promiseList.length) {
+                resolve(fullfild);
+            }
+        };
+
+        for(var i = 0; i < promiseList.length; i++){
+            (function(i){
+                promiseList[i]
+                    .then(function r (result) {
+                        fullfild[i] = result;
+                        checkResolving();
+                    })
+                    .catch(function() {reject()});
+            })(i)
+        }
+
+    });
+}
+
+var promiseArr = [
+    new Promise(function(resolve) {resolve(1)}),
+    new Promise(function(resolve) {setTimeout(function(){resolve(2)}, 2000)}),
+    new Promise(function(resolve) {resolve(3)})
+];
+
+var myProm = promiseAll(promiseArr);
+
+myProm.then(function(res) {console.log(res)});
